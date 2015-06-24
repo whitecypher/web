65f93c6776196b56bda3f56294b211ec3a31e8c2
Merten van Gerven <merten.vg@gmail.com>
2015-06-25 00:34:41 +0200

    Adjustments

--- webserve/webserve.go
+++ webserve/webserve.go
@@ -7,11 +7,10 @@ import (
 )
 
 type server struct {
-	started bool
-	done    chan bool
-	err     error
-	addr    string
-	server  *http.Server
+	addr   string
+	server *http.Server
+	err    error
+	done   chan bool
 }
 
 // Create a new webserver
@@ -27,24 +26,24 @@ func New(addr string, handler http.Handler) *server {
 	return ws
 }
 
+// Error returned by webserver exit
+func (ws *server) Err() error {
+	return ws.err
+}
+
 // Start the webserver
 func (ws *server) Start() *server {
 	go ws.start_concurrent()
 	return ws
 }
 
-// Get the error given by webserver on exit
-func (ws *server) Err() error {
-	return ws.err
-}
-
 // Internal webserver starter to be used as goroutine
 func (ws *server) start_concurrent() {
 	ws.err = gracehttp.Serve(ws.server)
-	ws.done <- true
+	close(ws.done)
 }
 
-// Wait the server to finish
-func (ws *server) Wait() bool {
-	return <-ws.done
+// Channel to wait on
+func (ws *server) Done() <-chan bool {
+	return ws.done
 }
